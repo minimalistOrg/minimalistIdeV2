b4198ea329a76b47ea9b486ccaee3382e95f1a94
Usiel Rivas <usielrivas00@gmail.com>
2022-04-19 11:27:04 -0600

    first case hover identifier working

--- src/component/RenderAST/ASTtype/Identifier.tsx
+++ src/component/RenderAST/ASTtype/Identifier.tsx
@@ -1,7 +1,7 @@
 function Identifier(props: { data: { name: string } }): JSX.Element {
   const data: { name: string } = props.data;
   // console.log(data)
-  return <span className={`p3 ast-identifier-${data.name}`}>{data.name}</span>;
+  return <span className={`p3 ast-identifier-${data.name}`} data-idhover={data.name}>{data.name}</span>;
 }
 
 export default Identifier;
--- src/component/RenderAST/ASTtype/VariableDeclaration.tsx
+++ src/component/RenderAST/ASTtype/VariableDeclaration.tsx
@@ -1,7 +1,9 @@
 import ChooseType from "../ChooseType";
-import {VariableDeclarationType} from "./interfaceType"
+import { VariableDeclarationType } from "./interfaceType";
 
-function VariableDeclaration(props:{data: VariableDeclarationType}):JSX.Element {
+function VariableDeclaration(props: {
+  data: VariableDeclarationType;
+}): JSX.Element {
   const data: VariableDeclarationType = props.data;
   // console.log(props.data);
 
@@ -9,7 +11,12 @@ function VariableDeclaration(props:{data: VariableDeclarationType}):JSX.Element 
     <>
       <span className="ast-variable-type">{data.kind}</span>
       <span> </span>
-      <span className="ast-variable-name" data-idhover={data.declarations[0].id.name}>{data.declarations[0].id.name}</span>
+      <span
+        className={`ast-variable-name ast-identifier-${data.declarations[0].id.name}`}
+        data-idhover={data.declarations[0].id.name}
+      >
+        {data.declarations[0].id.name}
+      </span>
       <span> </span>
       <span>=</span>
       <span> </span>
