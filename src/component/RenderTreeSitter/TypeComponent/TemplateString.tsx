16119b68b8d932b185df0a2d529279b99a0ed38a
Usiel Rivas <usielrivas00@gmail.com>
2022-05-17 08:57:53 -0600

    fixed bug Template string

--- src/component/RenderTreeSitter/TypeComponent/TemplateString.tsx
+++ src/component/RenderTreeSitter/TypeComponent/TemplateString.tsx
@@ -1,21 +1,38 @@
+import { useEffect, useState } from "react";
 import ChooseType from "../ChooseType";
 
-function TemplateString(props:any){
-  const data= props.data;
-  // console.log(data)
+function TemplateString(props: any) {
+  const data = props.data;
+  const [stringtxt, setStringtxt] = useState([]);
+  useEffect(() => {
+    let txt = data.text;
+    console.log(txt);
+    data.children.map((e: any, index: number) => {
+      if (index > 0 && index < data.children.length - 1) {
+        txt = txt.replace(e.text, ",");
+      }
+    });
+
+    txt = txt.substring(1, txt.length - 1);
+    txt = txt.split(",");
+    setStringtxt(txt);
+    console.log(txt);
+  }, []);
+
   return (
     <span className="TemplateString">
-      {
-        data.children.map((e:any,index:number)=>{
-          return (
-            <span key={index}>
+      {data.children.map((e: any, index: number) => {
+        return (
+          <span key={index}>
+            <>
               <ChooseType info={e} />
-            </span>
-          )
-      })
-      }
+              <span>{stringtxt[index]}</span>
+            </>
+          </span>
+        );
+      })}
     </span>
-  )
+  );
 }
 
 export default TemplateString;
