877b9a70ad47853a884e4ebb73f771f2d891bc09
Usiel Rivas <usielrivas00@gmail.com>
2022-07-16 07:52:45 -0600

    test for re opening bubble

---
+++ src/test/Others.spec.ts
@@ -0,0 +1,32 @@
+import { test, expect } from "@playwright/test";
+import {
+  OpenBubble,
+  CloseBubble,
+  OrderBubble,
+  HoverTree,
+  inputSearchTest,
+  HoverCallTree,
+  WithoutHoverCallTree,
+  HoverBubbleHeader,
+  BubbleHeader,
+  HoverParamsIdentifier,
+  ParamsIdentifier,
+} from "./util/function";
+
+const url_with_repo =
+  "http://localhost:5000/?repository=https://github.com/Usielrivas/CodeTest/tree/dbe5c82f601b73fe1dad662ef27bcb54c4d46100&data=eyJpIjowLCJ2IjpbeyJpIjo1LCJ2IjpbXX1dfQ==";
+
+test.describe.configure({ mode: "parallel" });
+
+test.describe("check to open bubbles when they are already open", () => {
+  test("from repo", async ({ page }) => {
+    await page.goto(url_with_repo);
+    await page.locator('[data-test-id="Bubble"]').nth(0).focus()
+    const bubbles = await page.locator('[data-test-id="Bubble"]').count();
+    expect(2).toEqual(bubbles);
+    await page.locator('[data-test-id="fncall"]').nth(1).focus()
+    OpenBubble(1, page);
+    const bubbles2 = await page.locator('[data-test-id="Bubble"]').count();
+    expect(2).toEqual(bubbles2);
+  });
+});
