1eb54adc9c6c38b25412e89f3c3b659e47060694
Daniel Garcia <pel_daniel@hotmail.com>
2022-08-15 17:12:48 -0500

    [Refactor] Fix reference from 'components' folder rename

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { Provider } from "react-redux";
 import { positions, Provider as AlertProvider } from "react-alert";
-import styleAlert from "./component/Root-file/root.module.css";
+import styleAlert from "./components/Root-file/root.module.css";
 import store from "./components/Root-file/Redux/store";
 
 import Root from "./components/Root-file/Root";
