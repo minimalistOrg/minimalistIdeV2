c4da930640712e0ffe32c3aff3df82beab939365
Daniel Garcia <pel_daniel@hotmail.com>
2022-08-16 16:32:14 -0500

    [Refactor] Remove semicolons from parser.ts

--- src/services/parser.ts
+++ src/services/parser.ts
@@ -1,5 +1,5 @@
-import { chooseLanguageGist } from "../components/Tree-Sitter/TreeSitter";
-import { CodeBlockCodeType } from "../types/interface";
+import { chooseLanguageGist } from "../components/Tree-Sitter/TreeSitter"
+import { CodeBlockCodeType } from "../types/interface"
 
 export const parser = {
   getAst: async (data: { code: string; from: string; language: string }[]) => {
@@ -15,9 +15,9 @@ export const parser = {
       })
     )
 
-    let result: CodeBlockCodeType[] = [].concat.apply([], treeSitterAst);
+    let result: CodeBlockCodeType[] = [].concat.apply([], treeSitterAst)
     result.forEach((e: CodeBlockCodeType, index: number) => {
-      e.id = index;
+      e.id = index
     })
 
     return result
