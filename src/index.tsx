2a7f421d4072a12caea2bd413df7961d8ef58eb3
Daniel Garcia <pel_daniel@hotmail.com>
2022-08-16 16:31:26 -0500

    [Refactor] Format index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,13 @@
-import * as ReactDOMClient from "react-dom/client";
-import { App } from "./App";
-import reportWebVitals from "./reportWebVitals";
+import * as ReactDOMClient from "react-dom/client"
+import { App } from "./App"
+import reportWebVitals from "./reportWebVitals"
 
-const container: any = document.getElementById("root");
+const container = document.getElementById("root")
 
-const root = ReactDOMClient.createRoot(container);
-root.render(<App />);
+const root = ReactDOMClient.createRoot(container!)
+root.render(<App />)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
