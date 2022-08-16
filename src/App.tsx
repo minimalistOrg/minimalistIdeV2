5593f43722502e45d177c152542dd15cfb469d90
Daniel Garcia <pel_daniel@hotmail.com>
2022-08-16 16:28:26 -0500

    [Refactor] Format App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,23 @@
-import { Provider } from "react-redux";
-import { positions, Provider as AlertProvider } from "react-alert";
-import store from "./components/Root-file/Redux/store";
+import { Provider } from "react-redux"
+import { positions, Provider as AlertProvider } from "react-alert"
+import store from "./components/Root-file/Redux/store"
 
-import { Root } from "./components/Root-file/Root";
-import { AlertTemplate } from "./components/AlertTemplate/AlertTemplate";
+import { Root } from "./components/Root-file/Root"
+import { AlertTemplate } from "./components/AlertTemplate/AlertTemplate"
 
 const options = {
   timeout: 5000,
   position: positions.BOTTOM_CENTER,
-};
+}
 
-function App(): JSX.Element {
+const App = () => {
   return (
     <Provider store={store}>
       <AlertProvider template={AlertTemplate} {...options}>
         <Root />
       </AlertProvider>
     </Provider>
-  );
+  )
 }
 
-export default App;
+export default App
